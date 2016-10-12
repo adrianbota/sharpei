e0f49f53868ca4650b2f129b25cdd7756fdd3959
Adrian Bota <adrianbotauk@gmail.com>
2016-10-12 23:59:36 +0100

    feed files from docs for gihub pages

--- gulpfile.js
+++ gulpfile.js
@@ -8,23 +8,24 @@ var autoprefix = new LessPluginAutoprefix({ browsers: ['last 2 versions'] });
 gulp.task('less', function () {
   gulp.src('src/sharpei/less/sharpei.less')
     .pipe(less({ plugins: [autoprefix] }))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('./docs'))
     .pipe(browserSync.stream());
 
   gulp.src('src/kitchen-sink/less/kitchen-sink.less')
     .pipe(less({ plugins: [autoprefix] }))
-    .pipe(gulp.dest('.'))
+    .pipe(gulp.dest('./docs'))
     .pipe(browserSync.stream());
 });
 
 gulp.task('html', function () {
   gulp.src('src/index.html')
     .pipe(fileInclude({ indent: true }))
-    .pipe(gulp.dest('.'));
+    .pipe(gulp.dest('./docs'));
 });
 
 gulp.task('dev', ['build'], function () {
-  browserSync({ server: { baseDir: '.' } });
+  browserSync({ server: { baseDir: './docs' } });
   gulp.watch('src/**/*.less', ['less']);
   gulp.watch('src/**/*.html', ['html']).on('change', browserSync.reload);
 });
